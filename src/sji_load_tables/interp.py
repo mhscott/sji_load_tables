c80f5014134529495c45adb1588b6af58af08559
denavit <denavit@gmail.com>
2023-08-13 20:49:02 -0400

    Adding docstring and removing examples

--- src/sji_load_tables/interp.py
+++ src/sji_load_tables/interp.py
@@ -1,7 +1,15 @@
 import bisect
 
 def interp(x,xp,fp,left=None,right=None):
+    '''Linear interpolation from a list
     
+    Arguments:
+        x: The input value
+        xp: The input list
+        fp: The output list
+        left: The value to return if x is less than the first value in xp (default None)
+        right: The value to return if x is greaterh than the last value in xp (default None)
+    '''
     # Value out of bounds
     if x < xp[0]:
         return left
@@ -14,18 +22,3 @@ def interp(x,xp,fp,left=None,right=None):
         
     i = bisect.bisect_left(xp,x)
     return fp[i-1] + (fp[i]-fp[i-1])*((x-xp[i-1])/(xp[i]-xp[i-1]))
-
-    
-def run_examples():
-    xp = [0,1,2,3,4,5]
-    fp = [0,2,-6,6,333,20]    
-    print(interp(-0.1,xp,fp))
-    print(interp(0,xp,fp))
-    print(interp(0.5,xp,fp))
-    print(interp(1.0,xp,fp))
-    print(interp(1.3,xp,fp))
-    print(interp(5,xp,fp))
-    print(interp(5.33,xp,fp))
-    
-if __name__ == '__main__':
-    run_examples()
