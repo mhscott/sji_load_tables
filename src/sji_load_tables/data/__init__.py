1e50f888de201b8d5be73a3343fce9d60c423ab5
denavit <denavit@gmail.com>
2023-08-28 20:40:43 -0400

    Improved data loading

--- src/sji_load_tables/data/__init__.py
+++ src/sji_load_tables/data/__init__.py
@@ -1,14 +1,23 @@
 import importlib.resources
 import json
+import sys
 
-try:
-    f = importlib.resources.read_text("sji_load_tables.data", "joist_data.json")
+if sys.version_info < (3, 9):
+    _read_text = importlib.resources.read_text
+else:
+    def _read_text(package, resource):
+        return importlib.resources.files(package).joinpath(resource).read_text()
+
+
+def load_data():
+    f = _read_text(__package__, 'joist_data.json')
     save_object = json.loads(f)
     joist_database = save_object['joist_database']
     joists_sorted_by_weight = save_object['joists_sorted_by_weight']
-    del f, save_object
-except:
-    import warnings
-    warnings.warn('joist_database did not load, setting to empty dict')
-    joist_database = dict()
-    joists_sorted_by_weight = []
+    return joist_database, joists_sorted_by_weight
+
+
+try:
+    joist_database, joists_sorted_by_weight = load_data()
+except Exception as exc:
+    raise RuntimeError("Failed to load joist database") from exc
